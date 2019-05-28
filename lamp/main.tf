9d2aa22412181ae267cbde8c12cced23f684c9a3
Prasanna T K Subramanian <contact@webplumbr.com>
2019-05-28 18:57:46 +1000

    adding IAM policy linked to a bucket

--- lamp/main.tf
+++ lamp/main.tf
@@ -5,3 +5,17 @@ provider "aws" {
 resource "aws_s3_bucket" "my_first_bucket" {
   bucket = "webplumbr-terraform-bucket-001"
 }
+
+resource "aws_iam_policy" "my_bucket_policy" {
+  name = "my-bucket-policy"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Action": ["s3:ListBucket"],
+    "Effect": "Allow",
+    "Resource": "${aws_s3_bucket.my_first_bucket.arn}"
+  }]
+}
+POLICY
+}
